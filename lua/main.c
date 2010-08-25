391acf0c266fabcd4fc2d702b7b4d2285f21d8b8
Daniel Aquino <mr.danielaquino@gmail.com>
2010-08-25 06:37:04 +0000

    more appropriate name

--- lua/main.c
+++ lua/main.c
@@ -1,7 +1,7 @@
 #include "lua_common.h"
 #include "lua_net_proxy.h"
 
-static int lua_set_args( lua_State* state, int argc, char ** argv )
+static int lua_set_argv( lua_State* state, int argc, char ** argv )
 {
 	int i, table;
 	lua_newtable(state);
@@ -23,7 +23,7 @@ int main( int argc, char ** argv )
 		return 1;
 	}
 	ASSERT(lua_init());
-	ASSERT(lua_set_args(L1,argc,argv));
+	ASSERT(lua_set_argv(L1,argc,argv));
 	ASSERT(lua_register_net_proxy());
 	ASSERT(lua_dofile(argv[1]));
 	return 0;
